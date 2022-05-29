28f12a338f83ddb79706b24c55fa06892c7d9e60
Maná Machado <manamachado8@gmail.com>
2022-05-29 17:28:56 -0300

    added endpoint

---
+++ api/product.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: 'Get data successfully'
+    })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send('Server error')
+  }
+})
+
+module.exports = router
